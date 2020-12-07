25315130b5376a8b0149103a6663486ab315f7a4
Капралов Александр <capralow@gmail.com>
2020-12-07 20:36:02 +0300

    Первая версия плагина

--- bundles/ru.capralow.dt.hrm.support.personnelaccounting_v3_1/src/ru/capralow/dt/hrm/support/internal/personnelaccounting_v3_1/BslModelUtils.java
+++ bundles/ru.capralow.dt.hrm.support.personnelaccounting_v3_1/src/ru/capralow/dt/hrm/support/internal/personnelaccounting_v3_1/BslModelUtils.java
@@ -4,6 +4,7 @@
 package ru.capralow.dt.hrm.support.internal.personnelaccounting_v3_1;
 
 import java.text.MessageFormat;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,11 +14,16 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.nodemodel.ILeafNode;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.DerivedStateAwareResource;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
 
+import com._1c.g5.v8.dt.bsl.model.BslPackage;
 import com._1c.g5.v8.dt.bsl.model.Conditional;
 import com._1c.g5.v8.dt.bsl.model.DynamicFeatureAccess;
 import com._1c.g5.v8.dt.bsl.model.EmptyStatement;
@@ -28,15 +34,19 @@ import com._1c.g5.v8.dt.bsl.model.FormalParam;
 import com._1c.g5.v8.dt.bsl.model.IfStatement;
 import com._1c.g5.v8.dt.bsl.model.Invocation;
 import com._1c.g5.v8.dt.bsl.model.Method;
+import com._1c.g5.v8.dt.bsl.model.Module;
 import com._1c.g5.v8.dt.bsl.model.SimpleStatement;
 import com._1c.g5.v8.dt.bsl.model.SourceObjectLinkProvider;
 import com._1c.g5.v8.dt.bsl.model.Statement;
 import com._1c.g5.v8.dt.bsl.model.StaticFeatureAccess;
 import com._1c.g5.v8.dt.bsl.model.StringLiteral;
+import com._1c.g5.v8.dt.bsl.resource.BslResolveCrossReferencesJob;
+import com._1c.g5.v8.dt.bsl.resource.BslResource;
 import com._1c.g5.v8.dt.bsl.resource.DynamicFeatureAccessComputer;
 import com._1c.g5.v8.dt.core.platform.IV8Project;
 import com._1c.g5.v8.dt.mcore.DerivedProperty;
 import com._1c.g5.v8.dt.mcore.Environmental;
+import com._1c.g5.v8.dt.mcore.McorePackage;
 import com._1c.g5.v8.dt.md.resource.MdTypeUtil;
 import com._1c.g5.v8.dt.metadata.mdclass.BasicDbObject;
 import com._1c.g5.v8.dt.metadata.mdclass.CommonModule;
@@ -48,6 +58,50 @@ public final class BslModelUtils
     private static DynamicFeatureAccessComputer dynamicFeatureAccessComputer =
         IResourceServiceProvider.Registry.INSTANCE.getResourceServiceProvider(URI.createURI("foo.bsl")).get( //$NON-NLS-1$
             DynamicFeatureAccessComputer.class);
+
+    public static void customResolveLazyCrossReferences(Resource resource)
+    {
+        BslResource bslResource = (BslResource)resource;
+
+        EcoreUtil2.resolveLazyCrossReferences(bslResource, null);
+        Collection<BslResolveCrossReferencesJob> jobs = BslResolveCrossReferencesJob.findJobsByResource(bslResource);
+        for (BslResolveCrossReferencesJob job : jobs)
+            try
+            {
+                job.join();
+            }
+            catch (InterruptedException e)
+            {
+                PersonnelAccountingPlugin.log(PersonnelAccountingPlugin.createErrorStatus(e.getMessage(), e));
+            }
+    }
+
+    public static INode getEndBracketParamsNode(Method method)
+    {
+        List<INode> nodesBefore = null;
+        if (!method.getFormalParams().isEmpty())
+            nodesBefore = NodeModelUtils.findNodesForFeature(method, BslPackage.Literals.METHOD__FORMAL_PARAMS);
+        else
+            nodesBefore = NodeModelUtils.findNodesForFeature(method, McorePackage.Literals.NAMED_ELEMENT__NAME);
+
+        if (nodesBefore.isEmpty())
+            return null;
+
+        int searchFrom = nodesBefore.get(nodesBefore.size() - 1).getTotalEndOffset();
+        INode methodNode = NodeModelUtils.findActualNodeFor(method);
+        INode bracketNode = null;
+        for (ILeafNode leafNode : methodNode.getLeafNodes())
+        {
+            if (leafNode.getOffset() < searchFrom || leafNode.isHidden() || leafNode.getLength() > 1)
+                continue;
+            if (")".equals(leafNode.getText())) //$NON-NLS-1$
+            {
+                bracketNode = leafNode;
+                break;
+            }
+        }
+        return bracketNode;
+    }
 
     private static void parseIfStatement(Statement statement, String variableName, List<String> objectsList,
         Map<String, String> modulesAliases, IV8Project v8Project)
@@ -81,7 +135,10 @@ public final class BslModelUtils
             if (commonModule == null)
                 return;
 
-            method = MdUtils.getMethod(commonModule.getModule(), dynamicMethodAccess.getName());
+            Module module = commonModule.getModule();
+            customResolveLazyCrossReferences(module.eResource());
+
+            method = MdUtils.getMethod(module, dynamicMethodAccess.getName());
             if (method == null)
                 return;
 
@@ -108,7 +165,12 @@ public final class BslModelUtils
         List<FeatureEntry> featureEntries = dynamicFeatureAccessComputer.resolveObject(staticMethodAccess,
             EcoreUtil2.getContainerOfType(staticMethodAccess, Environmental.class).environments());
         if (featureEntries.isEmpty())
-            return;
+        {
+            String errorMessage = "DynamicFeatureAccessComputer не смог получить FeatureEntries";
+            IllegalStateException exception = new IllegalStateException();
+            PersonnelAccountingPlugin.log(PersonnelAccountingPlugin.createErrorStatus(errorMessage, exception));
+            throw exception;
+        }
 
         FeatureEntry featureEntry = featureEntries.get(0);
         EObject feature = featureEntry.getFeature();
@@ -158,11 +220,20 @@ public final class BslModelUtils
                     List<FeatureEntry> featureEntries = dynamicFeatureAccessComputer.resolveObject(firstParam,
                         EcoreUtil2.getContainerOfType(firstParam, Environmental.class).environments());
                     if (featureEntries.isEmpty())
-                        return;
+                    {
+                        String errorMessage = "DynamicFeatureAccessComputer не смог получить FeatureEntries";
+                        IllegalStateException exception = new IllegalStateException();
+                        PersonnelAccountingPlugin.log(
+                            PersonnelAccountingPlugin.createErrorStatus(errorMessage, exception));
+                        throw exception;
+                    }
+
                     FeatureEntry featureEntry = featureEntries.get(0);
                     DerivedProperty deriveredProperty = (DerivedProperty)featureEntry.getFeature();
                     BasicDbObject objectOwner =
                         EcoreUtil2.getContainerOfType(deriveredProperty.getSource(), BasicDbObject.class);
+                    if (objectOwner == null)
+                        return;
 
                     objectsList.add(MdTypeUtil.getRefType(objectOwner).getName());
                 }
