18a8c2f5206205a8f783973edbfe391e65fe425b
Капралов Александр <capralow@gmail.com>
2020-12-08 14:20:11 +0300

    Первая версия плагина

--- bundles/ru.capralow.dt.hrm.support.personnelaccounting_v3_1/src/ru/capralow/dt/hrm/support/internal/personnelaccounting_v3_1/qlvalidator/PrivateMetadataQlModule.java
+++ bundles/ru.capralow.dt.hrm.support.personnelaccounting_v3_1/src/ru/capralow/dt/hrm/support/internal/personnelaccounting_v3_1/qlvalidator/PrivateMetadataQlModule.java
@@ -10,6 +10,7 @@ import com._1c.g5.v8.dt.ql.validation.QlValidator;
 public class PrivateMetadataQlModule
     extends AbstractGenericModule
 {
+    @org.eclipse.xtext.service.SingletonBinding(eager = true)
     public Class<? extends QlValidator> bindQlValidator()
     {
         return PrivateMetadataQlValidator.class;
--- bundles/ru.capralow.dt.hrm.support.personnelaccounting_v3_1/src/ru/capralow/dt/hrm/support/internal/personnelaccounting_v3_1/qlvalidator/PrivateMetadataQlValidator.java
+++ bundles/ru.capralow.dt.hrm.support.personnelaccounting_v3_1/src/ru/capralow/dt/hrm/support/internal/personnelaccounting_v3_1/qlvalidator/PrivateMetadataQlValidator.java
@@ -3,17 +3,23 @@
  */
 package ru.capralow.dt.hrm.support.internal.personnelaccounting_v3_1.qlvalidator;
 
+import static com._1c.g5.v8.dt.ql.model.QlPackage.Literals.ABSTRACT_QUERY_SCHEMA_TABLE__TABLE_DB_VIEW;
+
 import org.eclipse.xtext.validation.Check;
 
-import com._1c.g5.v8.dt.ql.model.QuerySchemaExpression;
+import com._1c.g5.v8.dt.ql.model.AbstractQuerySchemaTable;
 import com._1c.g5.v8.dt.ql.validation.QlValidator;
 
 public class PrivateMetadataQlValidator
     extends QlValidator
 {
     @Check
-    public void checkPersonnelHistory(QuerySchemaExpression qsExpr)
+    public void checkPersonnelHistory(AbstractQuerySchemaTable qsTable)
     {
-        return;
+        if (!qsTable.getFullTableName().equalsIgnoreCase("РегистрСведений.КадроваяИсторияСотрудников")) //$NON-NLS-1$
+            return;
+
+        error("Используйте программный интерфейс СоздатьВТКадровыеДанныеСотрудников вместо обращения к регистру",
+            qsTable, ABSTRACT_QUERY_SCHEMA_TABLE__TABLE_DB_VIEW);
     }
 }
